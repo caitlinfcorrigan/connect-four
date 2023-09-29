cf20d99d2bb167c301f219a9cf040f6d887d8a41
caitlincorrigan <caitlinfcorrigan@gmail.com>
2023-09-29 13:33:29 -0700

    updates first functions in js file

--- js/main.js
+++ js/main.js
@@ -3,11 +3,15 @@ console.log("hello, world!");
 
 
 	/*----- state variables -----*/
-// The current player/turn (1 or -1)
+
 // The board: 7 col; 6 rows --> two-dim array
+let board; //array of 7 column arrays
     // initialize with null -> no plays
     // array elems update with 1/-1 to represent the player's move
+    // The current player/turn (1 or -1)
+let turn;
 // Winner: null while in play; then 1/-1; for tie: "T"
+let winner;
 
 
 	/*----- cached elements  -----*/
@@ -17,3 +21,23 @@ console.log("hello, world!");
 
 
 	/*----- functions -----*/
+
+    // Initialize all state, then call render() - doesn't need input
+    function init(){
+        //initializing the let board var; rotate 90 deg counterclockwise to visualize the board in the DOM
+        board = [
+            [0, 0, 0, 0, 0, 0, 0] // col 0
+            [0, 0, 0, 0, 0, 0, 0] // col 1
+            [0, 0, 0, 0, 0, 0, 0] // col 2
+            [0, 0, 0, 0, 0, 0, 0] // col 3
+            [0, 0, 0, 0, 0, 0, 0] // col 4
+            [0, 0, 0, 0, 0, 0, 0] // col 5
+        ]; 
+        //game begins with Player 1
+        turn = 1;
+        winner = null;
+        render();
+    }
+    function render() {
+        
+    }
