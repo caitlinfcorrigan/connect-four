6833497b44d0e2b7a45b79684737beaa3766450a
caitlincorrigan <caitlinfcorrigan@gmail.com>
2023-09-29 12:18:58 -0700

    adds basic HTML and CSS from Part 2 video

--- js/main.js
+++ js/main.js
@@ -3,6 +3,11 @@ console.log("hello, world!");
 
 
 	/*----- state variables -----*/
+// The current player/turn (1 or -1)
+// The board: 7 col; 6 rows --> two-dim array
+    // initialize with null -> no plays
+    // array elems update with 1/-1 to represent the player's move
+// Winner: null while in play; then 1/-1; for tie: "T"
 
 
 	/*----- cached elements  -----*/
